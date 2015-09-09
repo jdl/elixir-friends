3501dbc0589cde3500d7a97bb5b24194fa2a9da6
JDL <jdl@workingasintended.com>
2015-09-09 14:30:37 -0500

    pulling db user/pass from the ENV for tests

--- config/test.exs
+++ config/test.exs
@@ -15,7 +15,7 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :elixir_friends, ElixirFriends.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "elixir_friends_test",
   pool: Ecto.Adapters.SQL.Sandbox
